81d505e7179e8480d934ce89f5a1c2c3051d9d7f
liqiang01 <liqiang01@inke.cn>
2020-01-30 22:38:11 +0800

    auto

---
+++ test03_stack.asm
@@ -0,0 +1,30 @@
+; stack practice
+assume cs: code, ds: data, ss: stack
+
+data segment
+    db 100 dup(0)
+data ends
+
+stack segment
+    db 200 dup(0)
+stack ends
+
+code segment
+start:   
+
+mov ax, 1122h
+mov bx, 3344h
+; 把 ax 的数据压入栈中
+push ax
+push bx
+; 把 栈顶（sp 指向的位置）的数据赋值给 ax
+pop ax
+pop bx
+       
+; 正常退出程序      
+mov ax, 4c00h
+int 21h
+
+code ends
+
+end start
